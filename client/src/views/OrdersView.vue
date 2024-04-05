dac56cedbb71353c2b142ee2d15aafd5b29f1f03
Ilvondir <komsa.m@o2.pl>
2024-04-05 12:00:18 +0200

    first version of orders screen created

--- client/src/views/OrdersView.vue
+++ client/src/views/OrdersView.vue
@@ -3,5 +3,63 @@ import BasePage from '@/components/pages/BasePage.vue'
 </script>
 
 <template>
-  <BasePage title="Zamówienia"> </BasePage>
+  <BasePage title="Dodaj zamówienie">
+    <v-row>
+      <v-col cols="12" lg="6">
+        <v-card class="mb-5"
+          variant="outlined"
+          title="Śrutownica"
+          subtitle="Wybierz z listy rowijanej maszynę, którą chce zamówić klient."
+        >
+          <v-select
+            class="ml-4 mr-4"
+            :items="['Śrutownica RB7', 'Śrutownica RB11']"
+            label="Wybierz maszynę:"
+          />
+        </v-card>
+
+        <v-card
+          variant="outlined"
+          title="Personalizacja"
+          subtitle="Wybierz elementy, które życzy sobie klient."
+        >
+          <v-checkbox class="ml-3"
+            hide-details
+            label="Hak holowniczy"
+            color="blue"
+          />
+          <v-checkbox class="ml-3"
+            label="System odsysania pyłów"
+            color="blue"
+            hide-details
+          />
+          <v-checkbox class="ml-3"
+            label="Dodatkowy zestaw kół"
+            color="blue"
+            hide-details
+          />
+        </v-card>
+
+      </v-col>
+      <v-col cols="12" lg="6">
+        <v-card class="mb-5"
+          title="Uwagi"
+          subtitle="Zapytaj klienta o dodatkowe uwagi."
+          variant="outlined"
+        >
+          <v-textarea class="ml-4 mr-4"
+            clearable
+          />
+        </v-card>
+        <v-card
+          title="Podsumowanie"
+          subtitle="Przedstaw klientowi wszystkie ustawiania i zapisz zamówienie."
+          variant="outlined"
+        >
+          <p class="ml-4 mr-4">Jeżeli klient potwierdza złożenia zamówienia, zapisz je w bazie danych.</p>
+          <v-btn color="blue" class="ma-3">Zapisz</v-btn>
+        </v-card>
+      </v-col>
+    </v-row>
+  </BasePage>
 </template>

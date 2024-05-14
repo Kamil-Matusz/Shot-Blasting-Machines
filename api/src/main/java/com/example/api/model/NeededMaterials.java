61bf3e2e6efe30a867779f86131a2cd639bd1369
Filip Lepa <fl122952@stud.ur.edu.pl>
2024-05-14 12:00:25 +0200

    Fixed n+1 problem for models and needed materials

--- api/src/main/java/com/example/api/model/NeededMaterials.java
+++ api/src/main/java/com/example/api/model/NeededMaterials.java
@@ -1,19 +1,22 @@
 package com.example.api.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
 @Data
 @Table(name="needed_materials")
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class NeededMaterials {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
-    @JsonBackReference
     @JoinColumn(name = "model_id", referencedColumnName = "id", nullable = false)
     private Model model;
 

f1c6e29d20e5f25e0963f4c47c5a6240e3789691
Filip Lepa <fl122952@stud.ur.edu.pl>
2024-05-31 21:52:56 +0200

    TP-43 added user data to the jwt token

--- api/src/main/java/com/example/api/config/CustomUserDetailsService.java
+++ api/src/main/java/com/example/api/config/CustomUserDetailsService.java
@@ -1,8 +1,10 @@
 package com.example.api.config;
 
+import com.example.api.dto.CustomUserDetails;
 import com.example.api.model.User;
 import com.example.api.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -17,25 +19,21 @@ import java.util.List;
  * Service for loading user-specific data.
  */
 @Service
-@RequiredArgsConstructor
 @Transactional
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
-    /**
-     * Loads the user details by username (email).
-     *
-     * @param email the email of the user to be loaded
-     * @return the {@link UserDetails} of the user
-     * @throws UsernameNotFoundException if the user with the given email is not found
-     */
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    public CustomUserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("User With Given Email Not Found!"));
-        String userRole = user.getRole().getName();
-        Collection<SimpleGrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(userRole));
-        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), authorities);
+        return new CustomUserDetails(user);
     }
+
+
 }
